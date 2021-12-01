8fc132c92c134c888cc1c5efee95e997ac206f0c
ShehjadSaif <shehjadsaif2004@gmail.com>
2021-12-01 19:31:53 +0600

    added firebase configuration

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyChyEKaWoB3zwpV9lPqhmK00JQoXUkpd-o",
+    authDomain: "jad-medicare.firebaseapp.com",
+    projectId: "jad-medicare",
+    storageBucket: "jad-medicare.appspot.com",
+    messagingSenderId: "413856807284",
+    appId: "1:413856807284:web:1936fb17ecc61e9f149421"
+};
+
+export default firebaseConfig;
